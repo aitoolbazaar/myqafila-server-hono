87a12f971c861c2946dd51e90301a9a148dc39c9
himanshu1-sharma <sharmagi1998@gmail.com>
2026-07-10 17:59:43 +0530

    feat: implement Vercel deployment support by adding a build entry point and esbuild configuration

--- vercel-entry.ts
+++ vercel-entry.ts
@@ -1,6 +1,6 @@
 import { getRequestListener } from "@hono/node-server";
-import app from "../src/app";
-import { connectDB } from "../src/config/db.config";
+import app from "./src/app";
+import { connectDB } from "./src/config/db.config";
 
 // Database connect verification (Runs on function cold start)
 connectDB().catch(console.error);
